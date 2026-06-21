d550d42ec5f7aa84aec488b1546585a0a7ac10bd
aditisaisrinivas-sudo <aditi.saisrinivas@gmail.com>
2026-06-21 02:37:38 +0000

    project 3 completed

--- PROJECT3.py
+++ PROJECT3.py
@@ -1,1 +1,114 @@
+import random
+n = random.randint(1,100)
+
+print('I have selected a number between 1 and 100. Can you guess?')
+
+
+attempts = 0
+
+done = False
+
+while not done:
+    guess = int(input('Guess the number\n'))
+
+
+    attempts = attempts + 1
+
+
+    if guess > n: 
+        print('My number is smaller than that.\n')
+    if guess < n: 
+        print('My number is greater than that.\n')
+
+    if guess == n:
+        print('Thats correct, Great job.')
+        print('You took ', attempts, 'attempts to guess it.')
+        done = True
+#now user selects a user
+print()
+print()
+
+print('now you select a number between 1 and 100')
+print('Click to enter when Ready')
+
+
+input()
+
+done=False
+attempts = 0
+guess_step = 10;
+prev_answer = 'l'
+
+while not done:
+    answer = input('Is it' + str(guess) + '?(y=yes, s=smaller than that, l = larger than that)\n')
+    attempts = attempts + 1
+
+
+    if attempts >1:
+        if answer !=prev_answer:
+            guess_step = guess_step - 1
+    
+    prev_answer = answer
+
+    if answer.lower() =='s':
+        guess = guess - guess_step
+
+    if answer.lower() =='l':
+        guess = guess + guess_step
+
+    if answer.lower() =='y':
+        print('You got it!')
+        print('I took', attempts, 'attempts to get it.')
+        done = True
+
+print()
+print()
+
+print('I think i can do it better')
+print('Let me try binary search...')
+
+
+#binary search
+
+done = False
+low = 0
+high = 100
+guess_step = 0
+attempts = 0
+
+
+while not done:
+    guess = round((low + high)/2)
+    answer = input('Is it '+ str(guess)+ '?(y=yes, s=smaller than that, l = larger than that)\n') 
+    attempts = attempts + 1
+
+    if answer.lower() == 's':
+        high = guess
+
+    if answer.lower() == 'l':
+        low = guess
+    
+    if answer.lower() == 'y':
+        print('I got it!')
+        print('I took', attempts, 'attempts to get it!')
+        done = True
+
+print()
+print()
+     
+
+
+
+    
+
+
+
+
+
+
+
+
+
+
+
 
